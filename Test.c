283f9e8089c315a6a0d92d9965d9be825dce23d3
SampreethNaik <44098709+SampreethNaik@users.noreply.github.com>
2019-07-08 11:33:53 +0530

    Master change-merge

--- Test.c
+++ Test.c
@@ -3,6 +3,7 @@ int main()
 {
     printf("Hello, This is Test");
     printf("Yo, What's Up?");
+    printf(" This is Master Testing\n");
     printf("More lines");
     printf("Test branch testing\n");
     return 0;
