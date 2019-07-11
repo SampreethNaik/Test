b826cfb4b535af27b670f683d2c607ed4e84017d
SampreethNaik <44098709+SampreethNaik@users.noreply.github.com>
2019-07-11 10:04:31 +0530

    Issue resolved as per #3

--- Test.c
+++ Test.c
@@ -7,6 +7,7 @@ int main()
     printf("More lines");
     printf("Test branch testing\n");
     printf("This is a change pertaining to issue");
+    printf("Issue was not solved");
     printf(" This is Test branch 2 test");
     return 0;
 }
